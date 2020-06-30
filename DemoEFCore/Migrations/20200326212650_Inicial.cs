edc3da7a64091135904eb69ec59837cb0d24fbc7
Icaro Henrique <icaro_henrick@hotmail.com>
2020-06-30 11:35:02 -0300

    Adicioando um projeto de revisão das novas funcionalidades do EntityFrameWorkCore

---
+++ DemoEFCore/Migrations/20200326212650_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoEFCore.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    Gratuito = table.Column<bool>(nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                    Data = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
